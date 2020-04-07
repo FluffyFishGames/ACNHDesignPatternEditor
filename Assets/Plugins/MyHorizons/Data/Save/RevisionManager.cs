da27b869328976bbd81bcdb5a10998e6b3e2cb1b
Philipp Mohrenstecher <philipp.mohrenstecher@jaeger.de>
2020-04-07 13:29:17 +0200

    Added 1.1.4 support

--- Assets/Plugins/MyHorizons/Data/Save/RevisionManager.cs
+++ Assets/Plugins/MyHorizons/Data/Save/RevisionManager.cs
@@ -57,7 +57,8 @@ namespace MyHorizons.Data.Save
             new SaveRevision(0x6D, 0x78, 0, 2, 1, 2, "1.1.0", 1, 1), // 1.1.0
             new SaveRevision(0x6D, 0x78, 0, 2, 2, 2, "1.1.1", 1, 1), // 1.1.1
             new SaveRevision(0x6D, 0x78, 0, 2, 3, 2, "1.1.2", 1, 1), // 1.1.2
-            new SaveRevision(0x6D, 0x78, 0, 2, 4, 2, "1.1.3", 1, 1)  // 1.1.3
+            new SaveRevision(0x6D, 0x78, 0, 2, 4, 2, "1.1.3", 1, 1), // 1.1.3
+            new SaveRevision(0x6D, 0x78, 2, 0, 2, 5, "1.1.4", 1, 1), // 1.1.4
         };
 
         // Table of save file sizes by revision
