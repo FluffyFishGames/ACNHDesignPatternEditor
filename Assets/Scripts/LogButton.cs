6feb7045ec085154fcb5671251b1afbc6a210771
FluffyFishGames <info@fluffyfish.de>
2020-04-20 21:24:58 +0200

    Changed unity version to 2019.3.10f1

--- Assets/Scripts/LogButton.cs
+++ Assets/Scripts/LogButton.cs
@@ -2,15 +2,78 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using UnityEngine;
+using System.IO;
 
 public class LogButton : MonoBehaviour
 {
+    public static void OpenInFileBrowser(string path)
+    {
+#if UNITY_STANDALONE_OSX
+        bool openInsidesOfFolder = false;
+
+        // try mac
+        string macPath = path.Replace("\\", "/"); // mac finder doesn't like backward slashes
+
+        if (Directory.Exists(macPath)) // if path requested is a folder, automatically open insides of that folder
+        {
+            openInsidesOfFolder = true;
+        }
+
+        //Debug.Log("macPath: " + macPath);
+        //Debug.Log("openInsidesOfFolder: " + openInsidesOfFolder);
+
+        if (!macPath.StartsWith("\""))
+        {
+            macPath = "\"" + macPath;
+        }
+        if (!macPath.EndsWith("\""))
+        {
+            macPath = macPath + "\"";
+        }
+        string arguments = (openInsidesOfFolder ? "" : "-R ") + macPath;
+        //Debug.Log("arguments: " + arguments);
+        try
+        {
+            System.Diagnostics.Process.Start("open", arguments);
+        }
+        catch (System.ComponentModel.Win32Exception e)
+        {
+            // tried to open mac finder in windows
+            // just silently skip error
+            // we currently have no platform define for the current OS we are in, so we resort to this
+            e.HelpLink = ""; // do anything with this variable to silence warning about not using it
+        }
+#endif
+#if UNITY_EDITOR || UNITY_STANDALONE_WIN
+        bool openInsidesOfFolder = false;
+
+        // try windows
+        string winPath = path.Replace("/", "\\"); // windows explorer doesn't like forward slashes
+
+        if (Directory.Exists(winPath)) // if path requested is a folder, automatically open insides of that folder
+        {
+            openInsidesOfFolder = true;
+        }
+        try
+        {
+            System.Diagnostics.Process.Start("explorer.exe", (openInsidesOfFolder ? "/root," : "/select,") + winPath);
+        }
+        catch (System.ComponentModel.Win32Exception e)
+        {
+            // tried to open win explorer in mac
+            // just silently skip error
+            // we currently have no platform define for the current OS we are in, so we resort to this
+            e.HelpLink = ""; // do anything with this variable to silence warning about not using it
+        }
+#endif
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         GetComponent<MenuButton>().OnClick = () => {
             string path = Application.persistentDataPath.TrimEnd(new[] { '\\', '/' }); // Mac doesn't like trailing slash
-            Process.Start(path);
+            OpenInFileBrowser(path);
         };
     }
 
